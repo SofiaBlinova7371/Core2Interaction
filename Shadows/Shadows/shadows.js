67b38061234915c2fa60d27743553e30b660f1ec
SofiaBlinova7371 <143515417+SofiaBlinova7371@users.noreply.github.com>
2024-02-15 12:29:07 -0500

    Add files via upload

---
+++ Shadows/Shadows/shadows.js
@@ -0,0 +1,36 @@
+const button = document.querySelector(".button");
+const links = document.querySelectorAll(".link");
+
+button.addEventListener("mouseover", () => {
+  links.forEach((link) => {
+    link.style.opacity = 1;
+    setTimeout(() => {
+      link.style.filter = "blur(0)";
+    }, 200);
+  });
+});
+
+button.addEventListener("mouseout", () => {
+  links.forEach((link) => {
+    link.style.filter = "blur(10px)";
+    setTimeout(() => {
+      link.style.opacity = 0;
+    }, 200);
+  });
+});
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    link.style.opacity = 1;
+    setTimeout(() => {
+      link.style.filter = "blur(0)";
+    }, 200);
+  });
+
+  link.addEventListener("mouseout", () => {
+    link.style.filter = "blur(10px)";
+    setTimeout(() => {
+      link.style.opacity = 0;
+    }, 200);
+  });
+});
